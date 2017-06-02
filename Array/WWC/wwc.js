e96ccf3b7c2dad6aa6e93960f27a597c5673ddd1
Trina Haque <trina_haque28@outlook.com>
2017-06-01 18:44:57 -0700

    rotate an array

--- Array/WWC/wwc.js
+++ Array/WWC/wwc.js
@@ -7,4 +7,21 @@ function printEventBackward(arr){
   }
 }
 
-console.log(printEventBackward([5,-2,3,2,8,-2,0,3]))
+// console.log(printEventBackward([5,-2,3,2,8,-2,0,3]))
+// output: 0,-2,8,2,-2
+
+
+function rotateArr(arr, n){
+  var r = n % arr.length;
+  // console.log("r is", r);
+  for (var i = 0; i < Math.floor(arr.length/2); i++){
+    // it's not i < r
+    var tmp = arr[r+i];
+    arr[r+i] = arr[i];
+    arr[i] = tmp;
+    // console.log(arr);
+  }
+  return arr;
+}
+
+console.log(rotateArr([1,2,3,4,5,6],1));
