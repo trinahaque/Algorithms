a01d3d8a8ad1f60143e891e388e158d280a495b7
Trina Haque <trina_haque28@outlook.com>
2017-08-02 18:14:30 -0700

    added Trie add method

--- Trie/trie4.js
+++ Trie/trie4.js
@@ -31,3 +31,27 @@ Trie_Node.prototype.insert = function(str){
     // if Trie already has some of the strings, add function will add the rest of the strings
     // if the word already exists, return false
 }
+
+
+Trie_Node.prototype.add = function(str){
+    // this function adds the whole strings to Trie, given the initial node of it
+    // adds bobny to bob
+    // this is node "b"
+    // str is "ny"
+    // adding a node with str value, not just the str
+    this.kids.push(new Trie_Node(str[0]));
+    if (str.length < 2){
+        // if it's the last character in the string
+        this.kids[0].isWord = true;
+        return true;
+    }
+    else{
+        // starting from the next string
+        // will return undefined without return statement
+        return this.kids[0].add(str.slice(1));
+    }
+}
+
+
+var trie = new Trie();
+console.log(trie.insert("bob"));
